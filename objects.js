78835d648e58ab5ede0c557640ff572b8e902dbe
Nelly Khmeleva <nkhmeleva@yahoo.com>
2019-04-07 03:40:23 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist={
   BritneySpears:'crazy',
   BackstreetBoys:'everybody',
   nsync:'promise'
-}
-function updatePlaylist(playlist,Christina,'Genie'){
+
+function updatePlaylist(playlist,Christina,Genie){
   return playlist;
 }
