3b9227e569a4f1dabffa74b13f6d04accdb26b45
Nelly Khmeleva <nkhmeleva@yahoo.com>
2019-04-07 03:54:12 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -8,7 +8,7 @@ function updatePlaylist(playlist, Christina, Genie){
   playlist[Christina]=Genie;
   return playlist;
 }
-function removeFromPlaylist(NSync,promise){
+function removeFromPlaylist(playlist,NSync){
   delete playlist.NSync;
   return playlist;
 }
